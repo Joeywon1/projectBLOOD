6685eae6c8e5dce0472c988afffd165a881618ee
Joey <113694544+Joeywon1@users.noreply.github.com>
2023-10-29 14:29:06 -0600

    Specials now destroy enemy bullets

--- enemy/enemy_bullet.gd
+++ enemy/enemy_bullet.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 var lifespan = 1.5
 var cooldownTime = 1.0 
-var bulletSpeed = 20
+var bulletSpeed = 10
 
 func _ready():
 	$Timer.wait_time = lifespan
@@ -12,3 +12,8 @@ func _process(delta):
 
 func _on_timer_timeout():
 	queue_free()
+
+
+func _on_area_entered(area):
+	if area.is_in_group("special"):
+		queue_free()
